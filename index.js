5e5b1ab097ad39c2b943a935d6e52fa94d2aa873
Talia Ritholtz <talia.ritholtz@baruchmail.cuny.edu>
2018-08-15 20:11:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ var greetSpecialGuest = "Hello," + "  Neil deGrasse Tyson" + " !";
 
 var topic = "space";
 
-var conversation = `${topic}` + "Let\'s talk about";
+var conversation = "Let\'s talk about " + `${topic}`;
